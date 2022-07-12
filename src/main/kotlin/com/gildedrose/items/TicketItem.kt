8b84e3f03a045ae365b116bf484833d76113f250
Mathias Cochet <mathias.cochet@rmdy.be>
2022-07-12 20:56:31 +0200

    Ticket item implemented.

--- src/main/kotlin/com/gildedrose/items/TicketItem.kt
+++ src/main/kotlin/com/gildedrose/items/TicketItem.kt
@@ -1,14 +1,26 @@
 package com.gildedrose.items
 
 import com.gildedrose.Actions
+import com.gildedrose.increaseQualityBy
 import com.gildedrose.items.base.Item
+import com.gildedrose.reduceSellInDate
 
-class TicketItem(name: String, sellIn: Int, quality: Int): Item(name, sellIn, quality), Actions {
+class TicketItem(name: String, sellIn: Int, quality: Int) : Item(name, sellIn, quality), Actions {
     override fun calculateItemSellIn() {
-        TODO("Not yet implemented")
+        reduceSellInDate(this, 1)
     }
 
     override fun calculateItemQuality() {
-        // calc
+        if (sellIn < 0) quality = 0
+        else increaseQualityBy(this, calculateIncrement());
+    }
+
+    private fun calculateIncrement(): Int {
+        return when {
+            sellIn > 10 -> 1
+            sellIn in 10 downTo 6 -> 2
+            sellIn in 5 downTo 0 -> 3
+            else -> throw IllegalStateException("This function cannot be called with a negative sellIn value");
+        }
     }
 }
