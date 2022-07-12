118cf5fd2c57a13bb16501fecd8bffe85988dc15
Mathias Cochet <mathias.cochet@rmdy.be>
2022-07-12 14:54:10 +0200

    Tests written for NormalItem quality update

--- src/main/kotlin/com/gildedrose/items/NormalItem.kt
+++ src/main/kotlin/com/gildedrose/items/NormalItem.kt
@@ -3,12 +3,9 @@ package com.gildedrose.items
 import com.gildedrose.Actions
 import com.gildedrose.items.base.Item
 
-class NormalItem(name: String, sellIn: Int, quality: Int): Item(name, sellIn, quality), Actions {
-    override fun calculateItemSellIn() {
-        sellIn--
-    }
+class NormalItem(name: String, sellIn: Int, quality: Int) : Item(name, sellIn, quality), Actions {
 
-    override fun calculateItemQuality(item: Item) {
-        TODO("Not yet implemented")
-    }
+    override fun calculateItemSellIn() {}
+
+    override fun calculateItemQuality(item: Item) {}
 }
--- src/test/kotlin/com/gildedrose/items/NormalItemTest.kt
+++ src/test/kotlin/com/gildedrose/items/NormalItemTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test
 internal class NormalItemTest {
 
     @Test
-    fun `test if sell in progresses the amount of days`() {
+    fun `Test if the sellIn property goes down every time the function is called`() {
         val item = NormalItem("Normal Item", 10, quality = 10)
 
         assertEquals(10, item.sellIn)
@@ -16,4 +16,52 @@ internal class NormalItemTest {
         assertEquals(9, item.sellIn)
     }
 
+    @Test
+    fun `Test that the sellIn property can go negative`() {
+        val item = NormalItem("Normal Item", 2, quality = 10)
+
+        assertEquals(2, item.sellIn)
+
+        item.calculateItemSellIn()
+
+        assertEquals(1, item.sellIn)
+
+        item.calculateItemSellIn()
+        item.calculateItemSellIn()
+
+        assertEquals(-1, item.sellIn)
+    }
+
+    @Test
+    fun `Test if quality reduces by one if sellIn is positive`() {
+        val item = NormalItem("Normal Item", 10, quality = 10)
+
+        assertEquals(10, item.quality)
+
+        item.calculateItemSellIn()
+
+        assertEquals(9, item.quality)
+    }
+
+    @Test
+    fun `Test if quality is reduced by 2 if sellIn is negative`() {
+        val item = NormalItem("Normal Item", -1, quality = 10)
+
+        assertEquals(10, item.quality)
+
+        item.calculateItemSellIn()
+
+        assertEquals(8, item.quality)
+    }
+
+    @Test
+    fun `Test if sellIn reaches 0, the quality only reduces by 1`() {
+        val item = NormalItem("Normal Item", 1, quality = 10)
+
+        assertEquals(10, item.quality)
+
+        item.calculateItemSellIn()
+
+        assertEquals(9, item.quality)
+    }
 }
