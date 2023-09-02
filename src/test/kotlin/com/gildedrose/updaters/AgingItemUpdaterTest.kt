fcb999cfc11a029933f87677b3569bfed1329cfd
Cochet <Mathias.Cochet@rmdy.be>
2023-09-02 16:18:21 +0200

    AgingItemUpdaterTest added

---
+++ src/test/kotlin/com/gildedrose/updaters/AgingItemUpdaterTest.kt
@@ -0,0 +1,54 @@
+package com.gildedrose.updaters
+
+import com.gildedrose.Item
+import com.gildedrose.itemupdaters.AgingItemUpdater
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class AgingItemUpdaterTest {
+
+    private lateinit var agingItemUpdater: AgingItemUpdater
+
+    @BeforeEach
+    fun setUp() {
+        agingItemUpdater = AgingItemUpdater()
+    }
+
+    @Test
+    fun `When running the updateItem function for an aging item the sellIn should decrease by 1`() {
+        val item = Item("Aging Item", 10, 10)
+
+        agingItemUpdater.updateItem(item)
+
+        assertEquals(9, item.sellIn)
+    }
+
+    @Test
+    fun `When running the updateItem function for an aging item the quality should increase by 1`() {
+        val item = Item("Aging Item", 10, 10)
+
+        agingItemUpdater.updateItem(item)
+
+        assertEquals(11, item.quality)
+    }
+
+    @Test
+    fun `When running the updateItem function for an aging item with a negative sellIn the quality should still increase by 1`() {
+        val item = Item("Aging Item", -2, 10)
+
+        agingItemUpdater.updateItem(item)
+
+        assertEquals(11, item.quality)
+    }
+
+    @Test
+    fun `When running the updateItem function for an aging item the quality should increase by 1 up until a quality of 50 is reached`() {
+        val item = Item("Aging Item", 10, 50)
+
+        agingItemUpdater.updateItem(item)
+
+        assertEquals(50, item.quality)
+    }
+
+}
