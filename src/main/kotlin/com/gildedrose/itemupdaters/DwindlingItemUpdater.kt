46f9400b9ec752da04972dce4d4b3b3c065b7177
Cochet <Mathias.Cochet@rmdy.be>
2023-09-02 17:08:12 +0200

    DwindlingItemUpdater added

--- src/main/kotlin/com/gildedrose/itemupdaters/DwindlingItemUpdater.kt
+++ src/main/kotlin/com/gildedrose/itemupdaters/DwindlingItemUpdater.kt
@@ -6,5 +6,7 @@ import com.gildedrose.itemupdaters.base.BaseItemUpdater
 class DwindlingItemUpdater : BaseItemUpdater() {
     override fun updateItem(item: Item) {
         super.updateItem(item)
+
+        item.quality = maxOf(item.quality - 2, MIN_QUALITY)
     }
 }
