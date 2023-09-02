a8bae8c0a7d1851d54b255cd404de430d50d1303
Cochet <Mathias.Cochet@rmdy.be>
2023-09-02 18:02:09 +0200

    EventItemUpdater added

--- src/main/kotlin/com/gildedrose/itemupdaters/EventItemUpdater.kt
+++ src/main/kotlin/com/gildedrose/itemupdaters/EventItemUpdater.kt
@@ -5,6 +5,13 @@ import com.gildedrose.itemupdaters.base.BaseItemUpdater
 
 class EventItemUpdater : BaseItemUpdater() {
     override fun updateItem(item: Item) {
+        when {
+            item.sellIn < 0 -> item.quality = MIN_QUALITY
+            item.sellIn in 0..5 -> item.quality = minOf(item.quality + 3, MAX_QUALITY)
+            item.sellIn in 6..10 -> item.quality = minOf(item.quality + 2, MAX_QUALITY)
+            else -> item.quality = minOf(item.quality + 1, MAX_QUALITY)
+        }
+
         super.updateItem(item)
     }
 }
