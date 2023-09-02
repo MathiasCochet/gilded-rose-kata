cf8eb4e1144809f110d806b44208996080aa268f
Cochet <Mathias.Cochet@rmdy.be>
2023-09-02 16:54:29 +0200

    DefaultItemUpdater added

--- src/main/kotlin/com/gildedrose/itemupdaters/DefaultItemUpdater.kt
+++ src/main/kotlin/com/gildedrose/itemupdaters/DefaultItemUpdater.kt
@@ -6,5 +6,11 @@ import com.gildedrose.itemupdaters.base.BaseItemUpdater
 class DefaultItemUpdater : BaseItemUpdater() {
     override fun updateItem(item: Item) {
         super.updateItem(item)
+
+        item.quality = if (item.sellIn >= 0) {
+            maxOf(item.quality - 1, MIN_QUALITY)
+        } else {
+            maxOf(item.quality - 2, MIN_QUALITY)
+        }
     }
 }
