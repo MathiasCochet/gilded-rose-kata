9dab1ade180a406123b1352b74bc84a49a4fe02e
Cochet <Mathias.Cochet@rmdy.be>
2023-09-06 19:20:22 +0200

    Fixed the EventItemUpdater logic

--- src/main/kotlin/com/gildedrose/itemupdaters/EventItemUpdater.kt
+++ src/main/kotlin/com/gildedrose/itemupdaters/EventItemUpdater.kt
@@ -5,13 +5,14 @@ import com.gildedrose.itemupdaters.base.BaseItemUpdater
 
 class EventItemUpdater : BaseItemUpdater() {
     override fun updateItem(item: Item) {
+        super.updateItem(item)
+
         when {
             item.sellIn < 0 -> item.quality = MIN_QUALITY
-            item.sellIn in 0..5 -> item.quality = minOf(item.quality + 3, MAX_QUALITY)
-            item.sellIn in 6..10 -> item.quality = minOf(item.quality + 2, MAX_QUALITY)
+            item.sellIn < 5 -> item.quality = minOf(item.quality + 3, MAX_QUALITY)
+            item.sellIn < 10 -> item.quality = minOf(item.quality + 2, MAX_QUALITY)
             else -> item.quality = minOf(item.quality + 1, MAX_QUALITY)
         }
 
-        super.updateItem(item)
     }
 }
