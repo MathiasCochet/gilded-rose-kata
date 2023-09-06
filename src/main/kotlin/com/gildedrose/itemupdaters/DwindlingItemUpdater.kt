aef9f2462bc44ae8ca4bdb5dcb6d9ad8aede76b4
Cochet <Mathias.Cochet@rmdy.be>
2023-09-06 19:22:25 +0200

    Changed the dwindlingItemUpdater to match the other updaters.

--- src/main/kotlin/com/gildedrose/itemupdaters/DwindlingItemUpdater.kt
+++ src/main/kotlin/com/gildedrose/itemupdaters/DwindlingItemUpdater.kt
@@ -5,8 +5,8 @@ import com.gildedrose.itemupdaters.base.BaseItemUpdater
 
 class DwindlingItemUpdater : BaseItemUpdater() {
     override fun updateItem(item: Item) {
-        item.quality = maxOf(item.quality - 2, MIN_QUALITY)
-
         super.updateItem(item)
+
+        item.quality = maxOf(item.quality - 2, MIN_QUALITY)
     }
 }
